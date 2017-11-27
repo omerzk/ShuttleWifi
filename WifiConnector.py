d523f953e3cda76fe80a6f727c4fe0abeb948426
omerzk <mine!>
2017-11-27 08:17:28 +0200

    works with correct pass, added modified wifi lib

--- WifiConnector.py
+++ WifiConnector.py
@@ -1,19 +1,23 @@
 import json
-from wifi import Cell, Scheme, exceptions
+import subprocess
 
-SHUTTLE_PASSWORDS = ["15383888", "15384888", "52347399", "31659399", "52354399", "52940399", "72983877", "74394344",
-                     "90136588", "90137588"]
+from wifi import Cell, Scheme, exceptions
+from pathlib import Path
 
 INTERFACE = "wlan0"
-SUTTLE_SSID = "Soofa"
+SHUTTLE_SSID = "Soofa"
+SHUTTLE_PASSWORDS = ["15383888", "15384888", "52347399", "31659399", "52354399", "52940399", "72983877", "74394344", "90136588", "90137588"]
 JSONFILE = "./knowMacToPassword.json"
 
 
 class ShuttleWiFiConnector:
 
     def __init__(self):
-        with open(JSONFILE, "r") as jsonfile:
-            self.MACToPassword = json.load(jsonfile)
+        if Path(JSONFILE).is_file():
+            with open(JSONFILE, "r") as jsonfile:
+                self.MACToPassword = json.load(jsonfile)
+        else:
+            self.MACToPassword = dict()
 
     def __del__(self):
         with open(JSONFILE, "w") as jsonfile:
@@ -25,17 +29,17 @@ class ShuttleWiFiConnector:
         cells = Cell.where(INTERFACE, ssidFilter)
 
         if not cells:
-            print("No", ssid, "network detected") #TODO: write prints logfile
+            print("No", ssid, "network detected") #TODO: write to logfile
             return None
 
         return max(cells, key=lambda cell: cell.quality)
 
     def tryConnect(self, cell, password):
-        schema = Scheme.for_cell(INTERFACE, SUTTLE_SSID, cell, password)
+        schema = Scheme.for_cell(INTERFACE, SHUTTLE_SSID, cell, password)
+        subprocess.call(["ifdown", "wlan0"])
         try:
-            schema.activate()
-        except exceptions.ConnectionError as e:
-            print("Failed to connect to", SHUTTLE_PASSWORDS, "with password", password)
+            subprocess.check_call(["ifup", "wlan0"] + schema.as_args())
+        except Exception as e:
             print(e)
             return False
 
@@ -44,16 +48,16 @@ class ShuttleWiFiConnector:
         return True
 
     def TryConnectShuttle(self):
-        cell = self.getBestCell(SUTTLE_SSID)
-
-        i = 0
         connected = False
+        cell = self.getBestCell(SHUTTLE_SSID)
 
-        if cell.address in self.MACToPassword:
-            connected = self.tryConnect(cell, self.MACToPassword[cell.address])
+        if cell:
+            i = 0
+            if cell.address in self.MACToPassword:
+                connected = self.tryConnect(cell, self.MACToPassword[cell.address])
 
-        while i < len(SHUTTLE_PASSWORDS) and not connected:
-            connected = self.tryConnect(cell, SHUTTLE_PASSWORDS[i])
-            i += 1
+            while i < len(SHUTTLE_PASSWORDS) and not connected:
+                connected = self.tryConnect(cell, SHUTTLE_PASSWORDS[i])
+                i += 1
 
         return connected
